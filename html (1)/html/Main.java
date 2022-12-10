ff748da5aa352f3b2ef120546215156a3781c71f
Rie <119394670+eggover@users.noreply.github.com>
2022-12-10 23:05:51 +0800

    Update Main.java

--- html (1)/html/Main.java
+++ html (1)/html/Main.java
@@ -1,110 +1,70 @@
-import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.io.RandomAccessFile;
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.nio.file.Path;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+import java.lang.*;
 
-public class Main {
 
-    private PDFParser extractData;
-    private PDFTextStripper stripper;
-    private PDDocument newPdf;
-    private COSDocument cosDoc;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-    private String textForm;
-    //   private String filePath;
-    private File file;
-
-    public Main() {
-    }
-
-    public String toText() throws IOException {
-        this.stripper = null;
-        this.newPdf = null;
-        this.cosDoc = null;
-
-        //set the location of the file
-        file = new File(setFilePath());
-        extractData = new PDFParser(new RandomAccessFile(file, "r")); //
-        update for PDFBox V 2.0
-
-        extractData.parse();
-
-        //place the extract text to temporary pdf file
-        cosDoc = extractData.getDocument();
-        stripper = new PDFTextStripper();
-
-        //to read the whole document from start page to last
-        newPdf = new PDDocument(cosDoc);
-        newPdf.getNumberOfPages();
-        stripper.setStartPage(0);
-        stripper.setEndPage(newPdf.getNumberOfPages());
-
-        //set the text to a variable and return it
-        textForm = stripper.getText(newPdf);
-        return textForm;
-    }
-
-    // read the file name from fileName.txt
-    public static String readTxtFile() {
-        String data = null;
+public class Main
+{
+    public static void main(String[]args) {
+        File inputFile = new File("C:/Users/Rie/Pictures/cat201/test.xlsx");
+        File outputFile = new File("C:/Users/Rie/Pictures/cat201/test.CSV");
+        StringBuilder data = new StringBuilder();
         try {
-            File txt = new File(Paths.get("").toAbsolutePath().toString() +
-                    "/fileName.txt");
-            Scanner readTXT = new Scanner(txt);
-            data = readTXT.nextLine();
-            readTXT.close();
+            FileInputStream fis = new FileInputStream(inputFile);
+            Workbook wb;
+            if (inputFile.getName().endsWith(".xlsx")) {
+                wb= new XSSFWorkbook(fis);
+            } else if (inputFile.getName().endsWith(".xls")) {
+                wb = new HSSFWorkbook(fis);
+            } else {
+                throw new Exception("File not supported!");
+            }
+            Sheet sheet = wb.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            if (rowIterator.hasNext()) {
+                do {
+                    Row row = rowIterator.next();
+                    Iterator<Cell> cellIterator = row.cellIterator();
+                    while (cellIterator.hasNext()) {
+                        Cell cell = cellIterator.next();
+                        switch (cell.getCellType()) {
+                            case BOOLEAN:
+                                data.append(cell.getBooleanCellValue()).append(",");
+                                break;
+                            case NUMERIC:
+                                data.append(cell.getNumericCellValue()).append(",");
+                                break;
+                            case STRING:
+                                data.append(cell.getStringCellValue()).append(",");
+                                break;
+                            case BLANK:
+                                data.append(cell).append(",");
+                            default:
+                                data.append(cell).append(",");
+                        }
+                    }
+                    data.append('\n');
+                } while (rowIterator.hasNext());
+            }
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            fos.write(data.toString().getBytes());
+            fos.close();
 
-        }
-        catch (FileNotFoundException e)  {
-            System.out.println("An error occured.");
-            e.printStackTrace();
-        }
-
-        return data;
+        } catch (Exception e)
+          {
+              e.printStackTrace();
+          }
+        System.out.println("Conversion of an Excel file to CSV file is done!");
     }
-    // the path to the uploadedFile folder contains uploaded file
-    public static String setFilePath() {
-        Path cwd = Paths.get("");
-        return  cwd.toAbsolutePath().toString() + "/uploadedFile/" +
-                readTxtFile() + ".pdf" ;
-    }
 
-    public static String createConvertedFile() {
-        Path cwd = Paths.get("");
-        return  cwd.toAbsolutePath().toString() + "/convertedFile/" +
-                readTxtFile() + "_converted.txt" ;
-    }
 
-    public static void writeToTXT(String file_name, String content) {
-
-        try {
-            FileWriter myWriter = new FileWriter(file_name);
-            myWriter.write(content);
-            myWriter.close();
-            System.out.println(" and file successfully converted.");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-//        }
-    }
-    public static void main(String[] args) throws IOException {
-        Main Main = new Main();
-
-        String namaFail = readTxtFile();
-
-        String text = Main.toText();
-
-        writeToTXT(createConvertedFile(), text);
-
-    }
 }
